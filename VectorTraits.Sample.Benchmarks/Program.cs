92a6f3976bb1832560eb8f1f03e5c58d05e3ce0f
zhouyuelin <as.zhouyuelin@aisino.com>
2024-11-11 17:40:19 +0800

    Add package: BenchmarkDotNet.

--- VectorTraits.Sample.Benchmarks/Program.cs
+++ VectorTraits.Sample.Benchmarks/Program.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using Zyl.VectorTraits;
 
 namespace Zyl.VectorTraits.Sample.Benchmarks {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Zyl.VectorTraits.Sample.Benchmarks");
+            TextWriter writer = Console.Out;
+            writer.WriteLine("Zyl.VectorTraits.Sample.Benchmarks");
             BenchmarksGlobal.Init();
+            writer.WriteLine();
+            // Run.
+            Architecture architecture = RuntimeInformation.OSArchitecture;
+            var config = DefaultConfig.Instance;
+            if (architecture == Architecture.X86 || architecture == Architecture.X64) {
+                config = config.AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(maxDepth: 3, printSource: true, printInstructionAddresses: true, exportGithubMarkdown: true, exportHtml: true)));
+            } else {
+                // Message: Arm64 is not supported (Iced library limitation)
+            }
+            config = config.AddJob(Job.MediumRun
+                .WithLaunchCount(1)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithId("InProcess"));
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly, config);
+            writer.WriteLine("Length={0}, {1}", summary.Length, summary);
         }
     }
 }
