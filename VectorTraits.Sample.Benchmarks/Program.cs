3c5e27137dbd7146576e1d37f7b90fde9cb2ac02
zhouyuelin <as.zhouyuelin@aisino.com>
2024-11-11 16:34:38 +0800

    Add project: VectorTraits.Sample.Benchmarks

---
+++ VectorTraits.Sample.Benchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Zyl.VectorTraits.Sample.Benchmarks {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Zyl.VectorTraits.Sample.Benchmarks");
+        }
+    }
+}
