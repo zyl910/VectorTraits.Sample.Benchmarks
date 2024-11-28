15f925a9f97b73f58a7dfedefd86ff4e7b269430
zhouyuelin <as.zhouyuelin@aisino.com>
2024-11-28 22:07:01 +0800

    Add ImageFlipYBenchmark.

---
+++ VectorTraits.Sample.Benchmarks.Inc/Image/ImageFlipYBenchmark.cs
@@ -0,0 +1,384 @@
+﻿#undef BENCHMARKS_OFF
+
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+#endif
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Zyl.VectorTraits;
+
+namespace Zyl.VectorTraits.Sample.Benchmarks.Image {
+#if BENCHMARKS_OFF
+    using BenchmarkAttribute = FakeBenchmarkAttribute;
+#else
+#endif // BENCHMARKS_OFF
+
+    /// <summary>
+    /// Flip the image vertically(FlipY) (对图像进行垂直翻转(FlipY)).
+    /// </summary>
+    public class ImageFlipYBenchmark : IDisposable {
+        private bool _disposed = false;
+        private static readonly Random _random = new Random(1);
+        private BitmapData _sourceBitmapData = null;
+        private BitmapData _destinationBitmapData = null;
+        private BitmapData _expectedBitmapData = null;
+
+        [Params(1024, 2048, 4096)]
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        ~ImageFlipYBenchmark() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing) {
+            if (_disposed) return;
+            _disposed = true;
+            if (disposing) {
+                Cleanup();
+            }
+        }
+
+        private BitmapData AllocBitmapData(int width, int height, PixelFormat format) {
+            const int strideAlign = 4;
+            if (width <= 0) throw new ArgumentOutOfRangeException($"The width({width}) need > 0!");
+            if (height <= 0) throw new ArgumentOutOfRangeException($"The width({height}) need > 0!");
+            int stride = 0;
+            switch (format) {
+                case PixelFormat.Format8bppIndexed:
+                    stride = width * 1;
+                    break;
+                case PixelFormat.Format24bppRgb:
+                    stride = width * 3;
+                    break;
+            }
+            if (stride <= 0) throw new ArgumentOutOfRangeException($"Invalid pixel format({format})!");
+            if (0 != (stride % strideAlign)) {
+                stride = stride - (stride % strideAlign) + strideAlign;
+            }
+            BitmapData bitmapData = new BitmapData();
+            bitmapData.Width = width;
+            bitmapData.Height = height;
+            bitmapData.PixelFormat = format;
+            bitmapData.Stride = stride;
+            bitmapData.Scan0 = Marshal.AllocHGlobal(stride * height);
+            return bitmapData;
+        }
+
+        private void FreeBitmapData(BitmapData bitmapData) {
+            if (null == bitmapData) return;
+            if (IntPtr.Zero == bitmapData.Scan0) return;
+            Marshal.FreeHGlobal(bitmapData.Scan0);
+            bitmapData.Scan0 = IntPtr.Zero;
+        }
+
+        [GlobalCleanup]
+        public void Cleanup() {
+            FreeBitmapData(_sourceBitmapData); _sourceBitmapData = null;
+            FreeBitmapData(_destinationBitmapData); _destinationBitmapData = null;
+            FreeBitmapData(_expectedBitmapData); _expectedBitmapData = null;
+        }
+
+        [GlobalSetup]
+        public void Setup() {
+            Height = Width;
+            // Create.
+            Cleanup();
+            _sourceBitmapData = AllocBitmapData(Width, Height, PixelFormat.Format24bppRgb);
+            _destinationBitmapData = AllocBitmapData(Width, Height, PixelFormat.Format24bppRgb);
+            _expectedBitmapData = AllocBitmapData(Width, Height, PixelFormat.Format24bppRgb);
+            RandomFillBitmapData(_sourceBitmapData, _random);
+
+            // Check.
+            bool allowCheck = true;
+            if (allowCheck) {
+                try {
+                    TextWriter writer = Console.Out;
+                    long totalDifference, countByteDifference;
+                    int maxDifference;
+                    double averageDifference;
+                    long totalByte = Width * Height * 3;
+                    double percentDifference;
+                    // Baseline
+                    ScalarDo(_sourceBitmapData, _expectedBitmapData);
+                    // ScalarParallel
+                    ScalarParallel();
+                    totalDifference = SumDifference(_expectedBitmapData, _destinationBitmapData, out countByteDifference, out maxDifference);
+                    averageDifference = (countByteDifference > 0) ? (double)totalDifference / countByteDifference : 0;
+                    percentDifference = 100.0 * countByteDifference / totalByte;
+                    writer.WriteLine(string.Format("Difference of ScalarParallel: {0}/{1}={2}, max={3}, percentDifference={4:0.000000}%", totalDifference, countByteDifference, averageDifference, maxDifference, percentDifference));
+                    // UseVectors
+                    UseVectors();
+                    totalDifference = SumDifference(_expectedBitmapData, _destinationBitmapData, out countByteDifference, out maxDifference);
+                    averageDifference = (countByteDifference > 0) ? (double)totalDifference / countByteDifference : 0;
+                    percentDifference = 100.0 * countByteDifference / totalByte;
+                    writer.WriteLine(string.Format("Difference of UseVectors: {0}/{1}={2}, max={3}, percentDifference={4:0.000000}%", totalDifference, countByteDifference, averageDifference, maxDifference, percentDifference));
+                    // UseVectorsParallel
+                    UseVectorsParallel();
+                    totalDifference = SumDifference(_expectedBitmapData, _destinationBitmapData, out countByteDifference, out maxDifference);
+                    averageDifference = (countByteDifference > 0) ? (double)totalDifference / countByteDifference : 0;
+                    percentDifference = 100.0 * countByteDifference / totalByte;
+                    writer.WriteLine(string.Format("Difference of UseVectorsParallel: {0}/{1}={2}, max={3}, percentDifference={4:0.000000}%", totalDifference, countByteDifference, averageDifference, maxDifference, percentDifference));
+                    // UseCopy
+                    UseCopy();
+                    totalDifference = SumDifference(_expectedBitmapData, _destinationBitmapData, out countByteDifference, out maxDifference);
+                    averageDifference = (countByteDifference > 0) ? (double)totalDifference / countByteDifference : 0;
+                    percentDifference = 100.0 * countByteDifference / totalByte;
+                    writer.WriteLine(string.Format("Difference of UseCopy: {0}/{1}={2}, max={3}, percentDifference={4:0.000000}%", totalDifference, countByteDifference, averageDifference, maxDifference, percentDifference));
+                    // UseCopyParallel
+                    UseCopyParallel();
+                    totalDifference = SumDifference(_expectedBitmapData, _destinationBitmapData, out countByteDifference, out maxDifference);
+                    averageDifference = (countByteDifference > 0) ? (double)totalDifference / countByteDifference : 0;
+                    percentDifference = 100.0 * countByteDifference / totalByte;
+                    writer.WriteLine(string.Format("Difference of UseCopyParallel: {0}/{1}={2}, max={3}, percentDifference={4:0.000000}%", totalDifference, countByteDifference, averageDifference, maxDifference, percentDifference));
+                } catch (Exception ex) {
+                    Debug.WriteLine(ex.ToString());
+                }
+            }
+            // Debug break.
+            //bool allowDebugBreak = false;
+            //if (allowDebugBreak) {
+            //    for (int i = 0; i < 10000; ++i) {
+            //        UseVectors();
+            //    }
+            //    Debugger.Break();
+            //    UseVectors();
+            //}
+        }
+
+        internal unsafe void RandomFillBitmapData(BitmapData bitmapData, Random random) {
+            if (null == bitmapData) return;
+            if (IntPtr.Zero == bitmapData.Scan0) return;
+            byte* pRow = (byte*)bitmapData.Scan0;
+            for (int i = 0; i < bitmapData.Height; i++) {
+                byte* p = pRow;
+                for (int j = 0; j < bitmapData.Stride; j++) {
+                    *p++ = (byte)random.Next(0x100);
+                }
+                pRow += bitmapData.Stride;
+            }
+        }
+
+        private unsafe long SumDifference(BitmapData expected, BitmapData dst, out long countByteDifference, out int maxDifference) {
+            const int cbPixel = 3; // Bgr24.
+            long totalDifference = 0;
+            countByteDifference = 0;
+            maxDifference = 0;
+            int maxPosX = -1, maxPosY = -1;
+            int width = expected.Width;
+            int height = expected.Height;
+            int strideSrc = expected.Stride;
+            int strideDst = dst.Stride;
+            byte* pRow = (byte*)expected.Scan0.ToPointer();
+            byte* qRow = (byte*)dst.Scan0.ToPointer();
+            for (int i = 0; i < height; i++) {
+                byte* p = pRow;
+                byte* q = qRow;
+                for (int j = 0; j < width; j++) {
+                    for (int k = 0; k < cbPixel; ++k) {
+                        int difference = Math.Abs((int)(*q) - *p);
+                        if (0 != difference) {
+                            totalDifference += difference;
+                            ++countByteDifference;
+                            if (maxDifference < difference) {
+                                maxDifference = difference;
+                                maxPosX = j;
+                                maxPosY = i;
+                            }
+                        }
+                        ++p;
+                        ++q;
+                    }
+                }
+                pRow += strideSrc;
+                qRow += strideDst;
+            }
+            if (maxDifference > 0) {
+                //Console.WriteLine(string.Format("SumDifference maxDifference={0}, at ({1}, {2})", maxDifference, maxPosX, maxPosY));
+            }
+            return totalDifference;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Scalar() {
+            ScalarDo(_sourceBitmapData, _destinationBitmapData, false);
+        }
+
+        [Benchmark]
+        public void ScalarParallel() {
+            ScalarDo(_sourceBitmapData, _destinationBitmapData, true);
+        }
+
+        public static unsafe void ScalarDo(BitmapData src, BitmapData dst, bool useParallel = false) {
+            int width = src.Width;
+            int height = src.Height;
+            int strideSrc = src.Stride;
+            int strideDst = dst.Stride;
+            byte* pSrc = (byte*)src.Scan0.ToPointer();
+            byte* pDst = (byte*)dst.Scan0.ToPointer();
+            bool allowParallel = useParallel && (height > 16) && (Environment.ProcessorCount > 1);
+            if (allowParallel) {
+                Parallel.For(0, height, i => {
+                    int start = i;
+                    int len = 1;
+                    byte* pSrc2 = pSrc + start * (long)strideSrc;
+                    byte* pDst2 = pDst + (height - 1 - start) * (long)strideDst;
+                    ScalarDoBatch(pSrc2, strideSrc, width, len, pDst2, strideDst);
+                });
+            } else {
+                ScalarDoBatch(pSrc, strideSrc, width, height, pDst, strideDst);
+            }
+        }
+
+        public static unsafe void ScalarDoBatch(byte* pSrc, int strideSrc, int width, int height, byte* pDst, int strideDst) {
+            int strideCommon = Math.Min(Math.Abs(strideSrc), Math.Abs(strideDst));
+            byte* pRow = pSrc;
+            byte* qRow = pDst + (height - 1) * (long)strideDst; // Set to last row.
+            for (int i = 0; i < height; i++) {
+                byte* p = pRow;
+                byte* q = qRow;
+                for (int j = 0; j < strideCommon; j++) {
+                    *q++ = *p++;
+                }
+                pRow += strideSrc;
+                qRow -= strideDst;
+            }
+        }
+
+        [Benchmark]
+        public void UseVectors() {
+            UseVectorsDo(_sourceBitmapData, _destinationBitmapData, false);
+        }
+
+        [Benchmark]
+        public void UseVectorsParallel() {
+            UseVectorsDo(_sourceBitmapData, _destinationBitmapData, true);
+        }
+
+        public static unsafe void UseVectorsDo(BitmapData src, BitmapData dst, bool useParallel = false) {
+            int vectorWidth = Vector<byte>.Count;
+            int width = src.Width;
+            int height = src.Height;
+            if (width <= vectorWidth) {
+                ScalarDo(src, dst, useParallel);
+                return;
+            }
+            int strideSrc = src.Stride;
+            int strideDst = dst.Stride;
+            byte* pSrc = (byte*)src.Scan0.ToPointer();
+            byte* pDst = (byte*)dst.Scan0.ToPointer();
+            bool allowParallel = useParallel && (height > 16) && (Environment.ProcessorCount > 1);
+            if (allowParallel) {
+                Parallel.For(0, height, i => {
+                    int start = i;
+                    int len = 1;
+                    byte* pSrc2 = pSrc + start * (long)strideSrc;
+                    byte* pDst2 = pDst + (height - 1 - start) * (long)strideDst;
+                    UseVectorsDoBatch(pSrc2, strideSrc, width, len, pDst2, strideDst);
+                });
+            } else {
+                UseVectorsDoBatch(pSrc, strideSrc, width, height, pDst, strideDst);
+            }
+        }
+
+        public static unsafe void UseVectorsDoBatch(byte* pSrc, int strideSrc, int width, int height, byte* pDst, int strideDst) {
+            int strideCommon = Math.Min(Math.Abs(strideSrc), Math.Abs(strideDst));
+            int vectorWidth = Vector<byte>.Count;
+            int maxX = strideCommon - vectorWidth;
+            byte* pRow = pSrc;
+            byte* qRow = pDst + (height - 1) * (long)strideDst; // Set to last row.
+            for (int i = 0; i < height; i++) {
+                Vector<byte>* pLast = (Vector<byte>*)(pRow + maxX);
+                Vector<byte>* qLast = (Vector<byte>*)(qRow + maxX);
+                Vector<byte>* p = (Vector<byte>*)pRow;
+                Vector<byte>* q = (Vector<byte>*)qRow;
+                for (; ; ) {
+                    Vector<byte> data;
+                    // Load.
+                    data = *p;
+                    // Store.
+                    *q = data;
+                    // Next.
+                    if (p >= pLast) break;
+                    ++p;
+                    ++q;
+                    if (p > pLast) p = pLast; // The last block is also use vector.
+                    if (q > qLast) q = qLast;
+                }
+                pRow += strideSrc;
+                qRow -= strideDst;
+            }
+        }
+
+        [Benchmark]
+        public void UseCopy() {
+            UseCopyDo(_sourceBitmapData, _destinationBitmapData, false);
+        }
+
+        [Benchmark]
+        public void UseCopyParallel() {
+            UseCopyDo(_sourceBitmapData, _destinationBitmapData, true);
+        }
+
+        public static unsafe void UseCopyDo(BitmapData src, BitmapData dst, bool useParallel = false) {
+            int width = src.Width;
+            int height = src.Height;
+            int strideSrc = src.Stride;
+            int strideDst = dst.Stride;
+            byte* pSrc = (byte*)src.Scan0.ToPointer();
+            byte* pDst = (byte*)dst.Scan0.ToPointer();
+            bool allowParallel = useParallel && (height > 16) && (Environment.ProcessorCount > 1);
+            if (allowParallel) {
+                Parallel.For(0, height, i => {
+                    int start = i;
+                    int len = 1;
+                    byte* pSrc2 = pSrc + start * (long)strideSrc;
+                    byte* pDst2 = pDst + (height - 1 - start) * (long)strideDst;
+                    UseCopyDoBatch(pSrc2, strideSrc, width, len, pDst2, strideDst);
+                });
+            } else {
+                UseCopyDoBatch(pSrc, strideSrc, width, height, pDst, strideDst);
+            }
+        }
+
+        public static unsafe void UseCopyDoBatch(byte* pSrc, int strideSrc, int width, int height, byte* pDst, int strideDst) {
+            int strideCommon = Math.Min(Math.Abs(strideSrc), Math.Abs(strideDst));
+            byte* pRow = pSrc;
+            byte* qRow = pDst + (height - 1) * (long)strideDst; // Set to last row.
+            for (int i = 0; i < height; i++) {
+                Buffer.MemoryCopy(pRow, qRow, strideCommon, strideCommon);
+                pRow += strideSrc;
+                qRow -= strideDst;
+            }
+        }
+
+    }
+
+}
+
+// == Benchmarks result
+
+// -- `.NET8.0` on Arm
+
+
+// -- `.NET8.0` on X86
+
+
+// -- `.NET Framework` on X86
